a55276a4878c116719ada01a389814fb09563a9b
MrKenobii <anldygc35@windowslive.com>
2022-10-15 18:44:42 +0300

    CRUD with MySQL

---
+++ Controllers/UserController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using User.DTO;
+using User.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace User.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly restContext DBContext;
+
+        public UserController(restContext DBContext)
+        {
+            this.DBContext = DBContext;
+        }
+        [HttpGet("GetUsers")]
+        public async Task<ActionResult<List<UserDTO>>> GetUser()
+        {
+            var List = await DBContext.Users.Select(
+                s => new UserDTO
+                {
+                    Id = s.Id,
+                    FirstName = s.FirstName,
+                    LastName = s.LastName,
+                    Username = s.Username,
+                    Password = s.Password,
+                    EnrollmentDate = s.EnrollmentDate
+                }
+            ).ToListAsync();
+
+            if (List.Count < 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return List;
+            }
+        }
+        [HttpGet("GetUserById")]
+        public async Task<ActionResult<UserDTO>> GetUserById(int Id)
+        {
+            UserDTO User = await DBContext.Users.Select(s => new UserDTO
+            {
+                Id = s.Id,
+                FirstName = s.FirstName,
+                LastName = s.LastName,
+                Username = s.Username,
+                Password = s.Password,
+                EnrollmentDate = s.EnrollmentDate
+            }).FirstOrDefaultAsync(s => s.Id == Id);
+            if (User == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return User;
+            }
+        }
+        [HttpPost("InsertUser")]
+        public async Task<HttpStatusCode> InsertUser(UserDTO User)
+        {
+            var entity = new Entities.User()
+            {
+                Id = User.Id,
+                FirstName = User.FirstName,
+                LastName = User.LastName,
+                Username = User.Username,
+                Password = User.Password,
+                EnrollmentDate = User.EnrollmentDate
+            };
+            DBContext.Users.Add(entity);
+            await DBContext.SaveChangesAsync();
+            return HttpStatusCode.Created;
+        }
+        [HttpPut("UpdateUser")]
+        public async Task<HttpStatusCode> UpdateUser(UserDTO User)
+        {
+            var entity = await DBContext.Users.FirstOrDefaultAsync(s => s.Id == User.Id);
+            entity.FirstName = User.FirstName;
+            entity.LastName = User.LastName;
+            entity.Username = User.Username;
+            entity.Password = User.Password;
+            entity.EnrollmentDate = User.EnrollmentDate;
+            await DBContext.SaveChangesAsync();
+            return HttpStatusCode.OK;
+        }
+        [HttpDelete("DeleteUser/{Id}")]
+        public async Task<HttpStatusCode> DeleteUser(int Id)
+        {
+            var entity = new  Entities.User()
+            {
+                Id = Id
+            };
+            DBContext.Users.Attach(entity);
+            DBContext.Users.Remove(entity);
+            await DBContext.SaveChangesAsync();
+            return HttpStatusCode.OK;
+        }
+        //GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
---
+++ Entities/restContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace User.Entities
+{
+    public partial class restContext : DbContext
+    {
+        public restContext()
+        {
+        }
+
+        public restContext(DbContextOptions<restContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseMySQL("server=localhost;port=3306;user=root;password=root;database=rest");
+//            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("user");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(45);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(45);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(45);
+
+                entity.Property(e => e.Username)
+                    .IsRequired()
+                    .HasMaxLength(45);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
